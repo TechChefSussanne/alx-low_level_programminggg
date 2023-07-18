96308496ae309d3971b70aae0595d5a5302d7920
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 16:03:34 -0700

    ./7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - Prints the last digit of a number
- * @n: The number to get the last digit from
+ * @n: The number
  *
  * Return: The value of the last digit
  */
@@ -11,10 +11,9 @@ int print_last_digit(int n)
 	int last_digit;
 
 	if (n < 0)
-		n = -n;
+		n = -n; /* Make n positive if it's negative */
 
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
-
+	last_digit = n % 10; /* Get the remainder when divided by 10 */
+	_putchar(last_digit + '0'); /* Convert the digit to character and print */
 	return (last_digit);
 }
