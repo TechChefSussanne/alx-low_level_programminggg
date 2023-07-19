d86eb47def0a5625628c4a69e32516d6fa1e60f5
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 17:30:35 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,47 +2,21 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n: Starting number
+ * @n: The starting number
  */
 void print_to_98(int n)
 {
-	/* Check if n is less than 98 */
-	if (n <= 98)
+	while (n != 98)
 	{
-		/* Print numbers from n to 98 */
-		while (n <= 98)
-		{
-			_putchar(n / 10 + '0'); /* Print tens digit */
-			_putchar(n % 10 + '0'); /* Print ones digit */
-
-			/* Print comma and space if n is not 98 */
-			if (n != 98)
-			{
-				_putchar(','); /* Print comma */
-				_putchar(' '); /* Print space */
-			}
-
-			n++; /* Increment n */
-		}
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+		_putchar(', ');
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
-	else
-	{
-		/* Print numbers from n to 98 */
-		while (n >= 98)
-		{
-			_putchar(n / 10 + '0'); /* Print tens digit */
-			_putchar(n % 10 + '0'); /* Print ones digit */
-
-			/* Print comma and space if n is not 98 */
-			if (n != 98)
-			{
-				_putchar(','); /* Print comma */
-				_putchar(' '); /* Print space */
-			}
-
-			n--; /* Decrement n */
-		}
-	}
-
-	_putchar('\n'); /* Print new line */
+	_putchar('9');
+	_putchar('8');
+	_putchar('\n');
 }
