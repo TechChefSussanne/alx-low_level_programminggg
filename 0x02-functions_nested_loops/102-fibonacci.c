b8846e3551dcd6712968b1a23cbd2ac815b27ae4
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 18:30:35 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,48 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * _putchar - Writes a character to the standard output
- * @c: The character to be written
- *
- * Return: On success 1
- * On error, -1 is returned and errno is set appropriately
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
-}
+	long int i, j, k, next;
 
-/**
- * print_fibonacci - Prints the first 50 Fibonacci numbers
- */
-void print_fibonacci(void)
-{
-	unsigned long int num1 = 1, num2 = 2, next;
-	int i;
+	j = 1;
 
-	_putchar('1' + '0');
-	_putchar(',');
-	_putchar(' ');
+	k = 2;
 
-	_putchar('2' + '0');
-	_putchar(',');
-	_putchar(' ');
-
-	for (i = 2; i < 50; i++)
+	for (i = 1; i <= 50; ++i)
 	{
-		next = num1 + num2;
-		_putchar((next / 10) + '0');
-		_putchar((next % 10) + '0');
-
-		if (i != 49)
+		if (j != 20365011074)
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%ld, ", j);
+		} else
+		{
+			printf("%ld\n", j);
 		}
-
-		num1 = num2;
-		num2 = next;
+		next = j + k;
+		j = k;
+		k = next;
 	}
 
-	_putchar('\n');
+	return (0);
 }
