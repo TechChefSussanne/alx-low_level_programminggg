02874ffaa9dcdab15b873b7d1a514a9a1a06309e
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 18:50:27 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,49 +1,89 @@
-/* File: fibonacci.c */
-
 #include "main.h"
 
 /**
- * print_fibonacci - Prints the first 98 Fibonacci numbers
+ * main - Entry point
+ *
+ * Description: Finds and prints the first 98 Fibonacci numbers,
+ *              starting with 1 and 2, followed by a new line.
+ * Return: Always 0 (Success)
  */
-void print_fibonacci(void)
+int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2, fib3;
-	int count;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
-	_putchar('1'); /* Print first Fibonacci number */
-	_putchar(',');
-	_putchar(' ');
+	j = 1;
+	k = 2;
 
-	_putchar('2'); /* Print second Fibonacci number */
-	_putchar(',');
-	_putchar(' ');
+	_putchar(j + '0'); /* Print the first Fibonacci number */
 
-	for (count = 3; count <= 98; count++)
+	for (i = 1; i < 91; i++)
 	{
-		fib3 = fib1 + fib2;
-		fib1 = fib2;
-		fib2 = fib3;
+		_putchar(','); /* Print the comma separator */
+		_putchar(' ');
 
-		print_number(fib3);
+		_putchar(k + '0'); /* Print the current Fibonacci number */
 
-		if (count != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		k = k + j;
+		j = k - j;
 	}
 
-	_putchar('\n');
+	/* Calculate and print the last 7 Fibonacci numbers */
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		_putchar(','); /* Print the comma separator */
+		_putchar(' ');
+
+		_putchar(k1 + '0'); /* Print the high part of the Fibonacci number */
+		print_number(k2); /* Print the low part of the Fibonacci number */
+
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	_putchar('\n'); /* Print a new line */
+
+	return (0);
 }
 
 /**
- * print_number - Prints a number
- * @n: The number to be printed
+ * print_number - Prints a number with leading zeros if necessary
+ * @number: The number to print
  */
-void print_number(unsigned long n)
+void print_number(unsigned long int number)
 {
-	if (n / 10)
-		print_number(n / 10);
+	if (number < 10)
+		_putchar('0'); /* Print leading zero if necessary */
 
-	_putchar((n % 10) + '0');
+	if (number < 100)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 1000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 10000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 100000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 1000000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 10000000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 100000000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	if (number < 1000000000)
+		_putchar('0'); /* Print leading zero if necessary */
+
+	_putchar(number + '0'); /* Print the number */
 }
