f767b6cc6fd73cce3561b1f5475d678ee4439b8c
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 18:22:21 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,50 +1,50 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a character to the standard output (stdout)
- * @c: The character to be written
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: On success 1.
- * On error, -1 is returned and errno is set appropriately.
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+  return (write(1, &c, 1));
 }
 
 /**
- * print_fibonacci - Prints the first 50 Fibonacci numbers
+ * print_fibonacci - prints the first 50 Fibonacci numbers
  */
 void print_fibonacci(void)
 {
-	int i;
-	unsigned long prev = 1, current = 2, next;
+  unsigned long int prev = 1, curr = 2, next, i;
 
-	_putchar('1');
-	_putchar(',');
-	_putchar(' ');
-	_putchar('2');
+  _putchar('1');
+  _putchar(',');
+  _putchar(' ');
+  _putchar('2');
+  _putchar(',');
+  _putchar(' ');
 
-	for (i = 3; i <= 50; i++)
-	{
-		next = prev + current;
-		_putchar(',');
-		_putchar(' ');
-		printf("%lu", next);
-		prev = current;
-		current = next;
-	}
-	_putchar('\n');
+  for (i = 0; i < 48; i++)
+  {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+
+    if (i != 47)
+    {
+      printf("%lu, ", next);
+    }
+    else
+    {
+      printf("%lu\n", next);
+    }
+  }
 }
 
-/**
- * main - Entry point of the program
- *
- * Return: 0 on success
- */
 int main(void)
 {
-	print_fibonacci();
-
-	return (0);
+  print_fibonacci();
+  return (0);
 }
