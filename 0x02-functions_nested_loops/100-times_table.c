c680154f24a79d9f03f1c2eef1c58d0793110c88
TechChefSussanne <sussannetiana@gmail.com>
2023-07-18 17:56:36 -0700

    ./100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print_times_table - Prints the n times table, starting with 0.
- * @n: The number for which times table is printed.
+ * @n: The number of times to print the table.
  *
  * Return: void
  */
@@ -10,48 +10,36 @@ void print_times_table(int n)
 {
 	int i, j, result;
 
-	/* Check if n is within the valid range */
 	if (n < 0 || n > 15)
 		return;
 
-	/* Iterate through each row */
 	for (i = 0; i <= n; i++)
 	{
-		/* Iterate through each column */
 		for (j = 0; j <= n; j++)
 		{
 			result = i * j;
 
-			/* Print the result */
 			if (j == 0)
-			{
-				_putchar('0');
-			}
+				_putchar('0' + result);
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
 
-			if (result < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(result + '0');
-			}
-			else if (result >= 10 && result < 100)
-			{
-				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			else if (result >= 100)
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
+				if (result < 10)
+					_putchar(' ');
+				else if (result >= 10 && result < 100)
+					_putchar('0' + result / 10);
+				else
+				{
+					_putchar('0' + result / 100);
+					_putchar('0' + (result % 100) / 10);
+				}
+
+				_putchar('0' + result % 10);
 			}
 		}
+
 		_putchar('\n');
 	}
 }
