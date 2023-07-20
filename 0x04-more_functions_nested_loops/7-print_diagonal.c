707e7f76862af8f7704bb5043bb26c298df67e99
TechChefSussanne <sussannetiana@gmail.com>
2023-07-20 06:19:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,20 +4,20 @@
  * print_diagonal - Draws a diagonal line on the terminal.
  * @n: Length of the diagonal.
  *
- * Description: This function prints a diagonal line on the terminal using the
- *              character '\'. The length of the diagonal is specified by the
- *              parameter 'n'.
- *
- * Return: void
+ * Description: Prints a diagonal line using the character '\'.
+ *              The length of the diagonal is specified by 'n'.
+ *              If 'n' is less than or equal to 0, nothing is printed.
  */
 void print_diagonal(int n)
 {
-    /* If 'n' is less than or equal to 0, this function does not print anything. */
+    /* Initialize variables for loop */
     int a, b;
 
+    /* Check if n is valid */
     if (n <= 0)
         _putchar('\n');
 
+    /* Loop to print diagonal line */
     for (a = 0; a < n; a++)
     {
         for (b = 0; b < a; b++)
